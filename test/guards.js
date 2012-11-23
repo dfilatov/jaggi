1643f50073ec6d58d8782aef544d71ed9f87ee23
Dmitry Filatov <dfilatov@yandex-team.ru>
2012-11-23 14:21:18 +0400

    nodeunit, istanbul and some tests added

---
+++ test/guards.js
@@ -0,0 +1,69 @@
+var jaggi = require('../lib/jaggi');
+
+var called;
+module.exports = {
+    setUp : function(callback) {
+        called = false;
+        callback();
+    },
+
+    'block should not be running if guard=false' : function(test) {
+        jaggi.run({
+            A : {
+                guard : false,
+                call : function() {
+                    called = true;
+                }
+            }
+        }).fin(function() {
+            test.strictEqual(called, false);
+            test.done();
+        });
+    },
+
+    'block should not be running if guard function return false' : function(test) {
+        jaggi.run({
+            A : {
+                guard : function() {
+                    return false;
+                },
+                call : function() {
+                    called = true;
+                }
+            }
+        }).fin(function() {
+            test.strictEqual(called, false);
+            test.done();
+        });
+    },
+
+    'block should be running if guard function return true' : function(test) {
+        jaggi.run({
+            A : {
+                guard : function() {
+                    return true;
+                },
+                call : function() {
+                    called = true;
+                }
+            }
+        }).fin(function() {
+            test.strictEqual(called, true);
+            test.done();
+        });
+    },
+
+    'block should be running if guard function return undefined' : function(test) {
+        jaggi.run({
+            A : {
+                guard : function() {},
+                call : function() {
+                    called = true;
+                }
+            }
+        }).fin(function() {
+            test.strictEqual(called, true);
+            test.done();
+        });
+    }
+};
