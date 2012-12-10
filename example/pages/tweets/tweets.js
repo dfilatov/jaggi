81303a5c2c4560cc1107de47e54f21463917c5b3
Dmitry Filatov <dfilatov@yandex-team.ru>
2012-12-10 17:05:40 +0400

    top block must be declared in input description

--- example/pages/tweets/tweets.js
+++ example/pages/tweets/tweets.js
@@ -1,36 +1,38 @@
 module.exports = {
     blocks : {
-        'trends' : {
-            include : 'includes/trends.js',
-            toState : function(_, ctx) {
-                return {
-                    trends : '.trends[:' + (ctx.request('limit') || 3)  + ']'
-                };
+        call : {
+            'trends' : {
+                include : 'includes/trends.js',
+                toState : function(_, ctx) {
+                    return {
+                        trends : '.trends[:' + (ctx.request('limit') || 3)  + ']'
+                    };
+                },
+                done : false
             },
-            done : false
-        },
 
-        'tweets' : {
-            deps : 'trends',
-            guard : 'trends',
-            timeout : 1000,
-            params : function(ctx) {
-                return { trends : ctx.state('trends') };
-            },
-            call : function(params) {
-                var subBlocks = {};
-                params.trends.forEach(function(trend) {
-                    subBlocks[trend.name] = {
-                        include : 'includes/tweets.js',
-                        params : function() {
-                            var res = this.__base();
-                            res.data = { q : trend.query };
-                            return res;
-                        },
-                        pointer : '.results[:3].text'
-                    }
-                });
-                return subBlocks;
+            'tweets' : {
+                deps : 'trends',
+                guard : 'trends',
+                timeout : 1000,
+                params : function(ctx) {
+                    return { trends : ctx.state('trends') };
+                },
+                call : function(params) {
+                    var subBlocks = {};
+                    params.trends.forEach(function(trend) {
+                        subBlocks[trend.name] = {
+                            include : 'includes/tweets.js',
+                            params : function() {
+                                var res = this.__base();
+                                res.data = { q : trend.query };
+                                return res;
+                            },
+                            pointer : '.results[:3].text'
+                        }
+                    });
+                    return subBlocks;
+                }
             }
         }
     }
